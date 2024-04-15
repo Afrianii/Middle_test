9f0d2cc7b68e3d4a3cf3fd1524dc69db28157a66
Afrianii <113485596+Afrianii@users.noreply.github.com>
2024-04-15 21:30:29 +0700

    Update SQLite_Part1.sql

--- SQLite_Part1.sql
+++ SQLite_Part1.sql
@@ -1,3 +1,19 @@
+### Tulis query jawaban pada online editor yang telah disarankan atau sesuai dengan preferensi ###
+
+### Notes: anda perlu membuat query table data terlebih dahulu dengan memasukkan query di bawah###
+CREATE TABLE employee_table (
+    employee TEXT NOT NULL,
+    salary INTEGER
+);
+
+INSERT INTO employee_table (employee, salary) VALUES
+('Alice', 11),
+('Benn', 43),
+('Charles', 33),
+('Dorothy', 55),
+('Emma', 22),
+('Franklin', 33);
+
 SELECT salary
 FROM employee_table
 ORDER BY salary DESC
